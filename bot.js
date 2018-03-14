e5da7f6de7711b7faeef6300bb5326cf25c2ff1c
Blieper <yoranmandema@eml.cc>
2018-03-14 21:51:40 +0100

    initial commit

---
+++ bot.js
@@ -0,0 +1,14 @@
+const discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Bot ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
