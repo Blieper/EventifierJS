cbbda0684188c9e1f722b399c6a90610a5ef9e3c
Blieper <yoranmandema@eml.cc>
2018-06-07 18:35:00 +0200

    roll the dice command

---
+++ modules/commands/fun.js
@@ -0,0 +1,23 @@
+exports.init = function (app) {
+
+    app.registerNamespace('fun', {
+        description: `This namespace is dedicated to random fun commands.`,
+    });
+
+    app.registerCommand('fun', 'rtd', {
+        pars: [
+            { name: 'sides', optional: true },
+        ],
+        description:
+            `Rolls a 6 sided dice. Number of sides can also be changed`
+    }, x => {
+        let sides = 6 || Math.max(x.sides,1);
+
+        let number = Math.ceil(Math.random * sides);
+
+        if (poll.hasvoted.indexOf(x.message.author.id) > -1) {
+            app.commandFeedback(x.message, "Result: " + number, {type: 'Rolling a dice!'});
+            return
+        }
+    });
+}
